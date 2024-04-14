ad181fca9ee06c2392fb6d8f21f8e0d3387cab1e
berners <matthewbernerslee@gmail.com>
2024-04-14 17:40:51 +0100

    alias finding refactor

--- lib/alias_import_require.ex
+++ lib/alias_import_require.ex
@@ -1,87 +1,85 @@
 defmodule DeadCodeFinder.AliasImportRequire do
   def find(file) do
-    file
-    |> String.split("\n", trim: true)
-    |> find(%{alias: [], import: [], require: []}, :single)
+    lines = String.split(file, "\n", trim: true)
+    find(%{aliases: [], imports: [], requires: [], mode: :single}, lines)
   end
 
-  defp find([], acc, _mode) do
+  defp find(acc, []) do
     acc
+    |> Map.delete(:mode)
+    |> Map.update!(:aliases, &Enum.reverse/1)
   end
 
-  defp find([line | rest], acc, mode) do
-    find(rest, acc, mode)
+  defp find(%{mode: mode} = acc, [line | rest]) do
+    alias_finders()
+    |> Map.fetch!(mode)
+    |> Enum.reduce_while(acc, fn {regex, regex_match_fun}, acc ->
+      case Regex.run(regex, line, capture: :all_but_first) do
+        nil ->
+          {:cont, acc}
 
-    alias_regexes()
-    |> Enum.reduce_while(nil, fn {regex, runnable_mode, new_mode}, _ ->
-      case {run_regex(regex, line, mode, runnable_mode), mode, new_mode} do
-        {nil, _, _} ->
-          {:cont, nil}
-
-        {[alias], :multi, _} ->
-          alias = acc.namespace <> "." <> alias
-          {:halt, {:add_aliases, [alias], new_mode}}
-
-        {[namespace], _, :multi} ->
-          acc = Map.put(acc, :namespace, namespace)
-          {:halt, {:acc, acc, new_mode}}
-
-        {[alias], _, _} ->
-          {:halt, {:add_aliases, [alias], new_mode}}
-
-        {[namespace, aliases], _, _} ->
-          {:halt, {:add_aliases, parse_multiple_aliases(namespace, aliases), new_mode}}
+        captures ->
+          {new_aliases, acc} = regex_match_fun.(captures, acc)
+          {:halt, %{acc | aliases: prepend_new_aliases(new_aliases, acc.aliases)}}
       end
     end)
-    |> case do
-      nil ->
-        find(rest, acc, mode)
-
-      {:acc, acc, new_mode} ->
-        new_mode = determine_new_mode(mode, new_mode)
-        find(rest, acc, new_mode)
-
-      {:add_aliases, aliases, new_mode} ->
-        new_mode = determine_new_mode(mode, new_mode)
-        aliases = Enum.map(aliases, fn alias -> Module.concat([alias]) end)
-        find(rest, Map.update!(acc, :alias, &(&1 ++ aliases)), new_mode)
-    end
+    |> find(rest)
   end
 
-  defp determine_new_mode(old_mode, :no_change), do: old_mode
-  defp determine_new_mode(_, new_mode), do: new_mode
-
-  defp run_regex(regex, line, _current_mode, :any) do
-    do_run_regex(regex, line)
-  end
-
-  defp run_regex(regex, line, current_mode, runnable_mode) do
-    if current_mode == runnable_mode do
-      do_run_regex(regex, line)
-    else
-      nil
-    end
-  end
-
-  defp do_run_regex(regex, line) do
-    Regex.run(regex, line, capture: :all_but_first)
-  end
-
-  defp parse_multiple_aliases(namespace, aliases) do
-    aliases
-    |> String.split(",")
-    |> Enum.map(fn alias ->
-      namespace <> "." <> String.trim(alias)
+  defp prepend_new_aliases(new_aliases, existing) do
+    Enum.reduce(new_aliases, existing, fn new_alias, aliases ->
+      [Module.concat([new_alias]) | aliases]
     end)
   end
 
-  defp alias_regexes do
-   [
-     {~r|^\s*alias ([^{]+)\.{([^}]+)}|, :single, :no_change},
-     {~r|^\s*alias ([^{\s,]+)\s*$|, :single, :no_change},
-     {~r|^\s*alias ([^{\s,]+)\.{$|, :single, :multi},
-     {~r|^\s*([^{\s,]+),$|, :multi, :multi},
-     {~r|^\s*([^{\s,]+)|, :multi, :single},
-   ]
+  defp alias_finders do
+    %{
+      single: [
+        single_line_single_alias(),
+        single_line_multiple_aliases(),
+        multi_line_alias_start_line()
+      ],
+      multi: [
+        multi_line_alias_middle_lines(),
+        multi_line_alias_last_line()
+      ]
+    }
+  end
+
+  defp single_line_single_alias do
+    {~r|^\s*alias ([^{\s,]+)\s*$|, fn [alias], acc -> {[alias], acc} end}
+  end
+
+  defp single_line_multiple_aliases do
+    {~r|^\s*alias ([^{]+)\.{([^}]+)}|,
+     fn [namespace, aliases], acc ->
+       aliases =
+         aliases
+         |> String.split(",")
+         |> Enum.map(fn alias -> namespace <> "." <> String.trim(alias) end)
+
+       {aliases, acc}
+     end}
+  end
+
+  defp multi_line_alias_start_line do
+    {~r|^\s*alias ([^{\s,]+)\.{$|,
+     fn [namespace], acc ->
+       {[], Map.merge(acc, %{namespace: namespace, mode: :multi})}
+     end}
+  end
+
+  defp multi_line_alias_middle_lines do
+    {~r|^\s*([^{\s,]+),$|,
+     fn [alias], acc ->
+       {[acc.namespace <> "." <> alias], acc}
+     end}
+  end
+
+  defp multi_line_alias_last_line do
+    {~r|^\s*([^{\s,]+)|,
+     fn [alias], acc ->
+       {[acc.namespace <> "." <> alias], Map.put(acc, :mode, :single)}
+     end}
   end
 end
--- test/alias_import_require_test.exs
+++ test/alias_import_require_test.exs
@@ -4,37 +4,49 @@ defmodule DeadCodeFiner.AliasImportRequireTest do
 
   describe "find/1" do
     test "given an empty file, there're no aliases etc" do
-      assert %{alias: [], import: [], require: []} == AliasImportRequire.find("")
+      assert %{aliases: [], imports: [], requires: []} == AliasImportRequire.find("")
     end
 
     test "given file with a simple one-line alias in it, we return it" do
       file = "defmodule DeadCodeFinder do\n  alias DeadCodeFinder.AliasImportRequire\n"
-      assert %{alias: [DeadCodeFinder.AliasImportRequire]} = AliasImportRequire.find(file)
+      assert %{aliases: [DeadCodeFinder.AliasImportRequire]} = AliasImportRequire.find(file)
     end
 
     test "given file with a different simple one-line alias in it, we return it" do
       file = "defmodule DeadCodeFinder do\n  alias Cool.Module\n"
-      assert %{alias: [Cool.Module]} = AliasImportRequire.find(file)
+      assert %{aliases: [Cool.Module]} = AliasImportRequire.find(file)
     end
 
     test "given file with a multiple simple one-line alias in it, we return it" do
       file = "defmodule DeadCodeFinder do\n  alias Cool.Module\n  alias Farm.Animals"
-      assert %{alias: [Cool.Module, Farm.Animals]} = AliasImportRequire.find(file)
+      assert %{aliases: [Cool.Module, Farm.Animals]} = AliasImportRequire.find(file)
     end
 
     test "given file with multiple aliases in one line using the curly brace syntax, we return them all" do
       file = "defmodule DeadCodeFinder do\n  alias Farm.Mammals.{Pig, Sheep}"
-      assert %{alias: [Farm.Mammals.Pig, Farm.Mammals.Sheep]} = AliasImportRequire.find(file)
+      assert %{aliases: [Farm.Mammals.Pig, Farm.Mammals.Sheep]} = AliasImportRequire.find(file)
     end
 
     test "with a multi-line alias, we get them all" do
-      file = "defmodule Zoom.Api.HTTP do\n  alias Farm.{\n    Mammals.Pig,\n    Mammals.Sheep,\n    Barnyard,\n    Tractor,\n    Fields\n  }\n\n  # We are using the Turtles Zoom meeting ID to regularly check that\n "
-      assert %{alias: [Farm.Mammals.Pig, Farm.Mammals.Sheep, Farm.Barnyard, Farm.Tractor, Farm.Fields]} = AliasImportRequire.find(file)
+      file =
+        "defmodule Zoom.Api.HTTP do\n  alias Farm.{\n    Mammals.Pig,\n    Mammals.Sheep,\n    Barnyard,\n    Tractor,\n    Fields\n  }\n\n  # We are using the Turtles Zoom meeting ID to regularly check that\n "
+
+      assert %{
+               aliases: [
+                 Farm.Mammals.Pig,
+                 Farm.Mammals.Sheep,
+                 Farm.Barnyard,
+                 Farm.Tractor,
+                 Farm.Fields
+               ]
+             } = AliasImportRequire.find(file)
     end
 
     test "with a one-line alias thats wrong because there's nonsense at the end, we don't return it" do
-      file = "defmodule DeadCodeFinder do\n  alias DeadCodeFinder.AliasImportRequire.liulkjshldfsli,{\n"
-      assert %{alias: []} = AliasImportRequire.find(file)
+      file =
+        "defmodule DeadCodeFinder do\n  alias DeadCodeFinder.AliasImportRequire.liulkjshldfsli,{\n"
+
+      assert %{aliases: []} = AliasImportRequire.find(file)
     end
   end
 end
